4ef9a99c8a19759427d3fba121149254c6f9bf66
Josh Manders <josh@joshmanders.com>
2019-07-18 11:42:11 -0500

    fix vulnerabilities in deps and update xo

--- test/repl.js
+++ test/repl.js
@@ -2,16 +2,16 @@
 
 require('assert');
 
-var should = require('should');
-var Vantage = require('vorpal');
-var repl = require('./../lib/repl');
+const should = require('should');
+const Vantage = require('vorpal');
+const repl = require('./../lib/repl');
 
-var vorpal;
-var stdout = '';
+let vorpal;
+let stdout = '';
 
 global.foo = 'bar';
 global.blob = {
-  json: {'uncle': 'sam', 'aunt': 'mary'},
+  json: {uncle: 'sam', aunt: 'mary'},
   circular: {
     gotcha: undefined,
     innocent: 'bystander'
@@ -24,61 +24,61 @@ function stdoutFn(data) {
   return '';
 }
 
-describe('vorpal-repl', function () {
-  describe('root', function () {
-    before('vorpal preps', function () {
+describe('vorpal-repl', () => {
+  describe('root', () => {
+    before('vorpal preps', () => {
       vorpal = new Vantage().pipe(stdoutFn).show();
     });
 
-    beforeEach('vorpal preps', function () {
+    beforeEach('vorpal preps', () => {
       stdout = '';
     });
 
-    it('should exist and be a function', function () {
+    it('should exist and be a function', () => {
       should.exist(repl);
       repl.should.be.type('function');
     });
 
-    it('should import into Vantage', function () {
+    it('should import into Vantage', () => {
       (function () {
         vorpal.use(repl);
       }).should.not.throw();
     });
 
-    it('should log into REPL', function (done) {
-      vorpal.exec('repl', function (err, data) {
+    it('should log into REPL', done => {
+      vorpal.exec('repl', (err, data) => {
         should.not.exist(err);
         data.should.containEql('Entering REPL Mode.');
         done();
       });
     });
 
-    it('should execute JS', function (done) {
-      vorpal.exec('Math.pow(6 * 6, 2);', function (err, data) {
+    it('should execute JS', done => {
+      vorpal.exec('Math.pow(6 * 6, 2);', (err, data) => {
         should.not.exist(err);
         data.should.equal(1296);
         done();
       });
     });
 
-    it('should have access to global', function (done) {
-      vorpal.exec('foo', function (err, data) {
+    it('should have access to global', done => {
+      vorpal.exec('foo', (err, data) => {
         should.not.exist(err);
         data.should.equal('bar');
         done();
       });
     });
 
-    it('should stringify JSON', function (done) {
-      vorpal.exec('blob.json', function (err) {
+    it('should stringify JSON', done => {
+      vorpal.exec('blob.json', err => {
         should.not.exist(err);
         stdout.replace(/\n/g, '').replace(/\\/g, '').should.equal('{  "uncle": "sam",  "aunt": "mary"}');
         done();
       });
     });
 
-    it('should nail circular references like a boss', function (done) {
-      vorpal.exec('blob.circular', function (err) {
+    it('should nail circular references like a boss', done => {
+      vorpal.exec('blob.circular', err => {
         should.not.exist(err);
         done();
       });
